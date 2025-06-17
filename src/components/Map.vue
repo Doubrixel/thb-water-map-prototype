976b8da40751bc6e1b0df8c0e5a5c641cae79301
Florian Lemnitzer <florian.lemnitzer@data-experts.de>
2025-06-17 19:02:36 +0200

    feat: legende

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mapHolder">
-    <l-map ref="map" v-model:zoom="zoom" :center="[52.41604, 12.60930]" :use-global-leaflet="false">
+    <l-map ref="map" v-model:zoom="zoom" :center="[52.41604, 12.60930]" :use-global-leaflet="false" @ready="onMapReady">
       <l-tile-layer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           layer-type="base"
@@ -18,6 +18,7 @@
 </template>
 
 <script>
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import {LMap, LMarker, LTileLayer, LTooltip, LPopup, LGeoJson} from "@vue-leaflet/vue-leaflet";
 import SondenMarker from "@/components/SondenMarker.vue";
@@ -49,6 +50,33 @@ export default {
         weight: 1,
         fillOpacity: 0.1
       };
+    },
+    addLegend() {
+      const legend = L.control({ position: "bottomright" });
+      legend.onAdd = function () {
+        const div = L.DomUtil.create("div", "info legend");
+        div.style.background = "white";       // weißer Hintergrund hinter allem
+        div.style.padding = "6px 8px";        // etwas Padding für Abstand
+        div.style.borderRadius = "4px";       // leichte Rundung für schöneres Aussehen
+        div.style.boxShadow = "0 0 15px rgba(0,0,0,0.2)"; // leichter Schatten
+
+        div.innerHTML = `
+    <i style="
+      border: 1px solid blue;    /* blauer Rand */
+      background-color: rgba(0, 0, 255, 0.2); /* transparent innen */
+      width: 18px;
+      height: 18px;
+      display: inline-block;
+      margin-right: 8px;
+      vertical-align: middle;
+    "></i> Polder
+  `;
+        return div;
+      };
+      legend.addTo(this.$refs.map.leafletObject);
+    },
+    onMapReady() {
+      this.addLegend();
     }
   }
 };
